6b5dbb1cbda9e1bd9bebf42b95777fcdd65a0da0
Divya Bairwa <98870450+DivyaBairwa@users.noreply.github.com>
2023-11-17 15:23:22 +0530

    api-testing-done

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+import { fetchDataFromApi } from "./utils/api";
 
 function App() {
-  return (
-    <>
-      <div className="App"> App</div>
-    </>
-  );
+  useEffect(() => {
+    apitesting();
+  }, []);
+
+  const apitesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+    });
+  };
+  return <div className="App"> App</div>;
 }
 
 export default App;
